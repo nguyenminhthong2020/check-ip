27b47d8af86ec87295bd390ff36b14fc085f195c
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-08-04 01:44:15 +0700

    check location

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
-const express = require("express")
-const cors = require('cors');
-const ping = require ("net-ping");
-const requestIp = require('request-ip');
-const morgan = require('morgan');
-require('express-async-errors');
+const express = require("express");
+const cors = require("cors");
+//const ping = require("ping");
+const geoip = require("geoip-lite");
+const requestIp = require("request-ip");
+const morgan = require("morgan");
+require("express-async-errors");
 
 const port = process.env.PORT || 5000;
 
-
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 // Index - Home
-app.get('/', (req, res) => { 
-    let clientIp = requestIp.getClientIp(req);            
-    // res.json(`IP của bạn là ${clientIp}`);
+app.get("/", async (req, res) => {
+  let clientIp = requestIp.getClientIp(req);
 
-    const session = ping.createSession ();
-
-    session.pingHost (clientIp, function (error, target) {
-    if (error)
-        res.status(200).send("Thất bại !");
-    else
-    const result = {
-        clientIp,
-        info: target
-    };
-     res.status(200).send(result);
-   });
-
-
-})
-
-
-
-app.use((req, res, next) => {              // default route
-    res.status(404).send('ROUTE NOT FOUND');
-  })
+  var ip = clientIp;
+  var thongtin = geoip.lookup(ip);
   
-app.use(function (err, req, res, next) {        // default error-handler
-    if (typeof err.status === 'undefined' || err.status === 500) {
-        console.error(err.stack);
-        res.status(500).send('View error log on console.');
-      } else {
-        res.status(err.status).send(err);
-      }
-})
+  let result = {
+      ip,
+      thongtin
+  }
+  
+  res.status("200").send(result);
+});
 
-app.listen(port,()=>{
-    console.log(`API is running at http://localhost:${port}`)
-})
+app.use((req, res, next) => {
+  // default route
+  res.status(404).send("ROUTE NOT FOUND");
+});
+
+app.use(function (err, req, res, next) {
+  // default error-handler
+  if (typeof err.status === "undefined" || err.status === 500) {
+    console.error(err.stack);
+    res.status(500).send("View error log on console.");
+  } else {
+    res.status(err.status).send(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API is running at http://localhost:${port}`);
+});
